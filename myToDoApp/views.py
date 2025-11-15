47919ce14f064a81c96836e2cd39b0279a6a3308
jatin <jatin2372005@gmail.com>
2025-11-15 14:59:34 +0530

    n

--- myToDoApp/views.py
+++ myToDoApp/views.py
@@ -1,42 +1,44 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from myToDoApp import models
+from django.contrib.auth import authenticate, login as auth_login
 from myToDoApp.models import TODO
+
 
 def signup(request):
     if request.method == 'POST':
         fnm = request.POST.get('fnm')
         email = request.POST.get('email')
         password = request.POST.get('pwd')
-        print(fnm, email, password)
-        my_user=User.objects.create_user(fnm,email,password)
-        my_user.save()
+
+        User.objects.create_user(username=fnm, email=email, password=password)
         return redirect('/login')
     return render(request, 'signup.html')
 
-def login(request):
+
+def login_view(request):
     if request.method == 'POST':
         fnm = request.POST.get('fnm')
         password = request.POST.get('pwd')
-        print(fnm, password)
-        user = authentication(request, username = fnm, password = password)
-        if user is not None :
-            login (request, user)
+
+        user = authenticate(request, username=fnm, password=password)
+
+        if user is not None:
+            auth_login(request, user)
             return redirect('/todo')
-        else : 
+        else:
             return redirect('/login')
+
     return render(request, 'login.html')
 
+
 def todo(request):
+    if not request.user.is_authenticated:
+        return redirect('/login')
+
     if request.method == "POST":
         title = request.POST.get('title')
-        print(title)
-        obj = models.TODO(title=title, user=request.user)
-        obj.save()
-        res = models.TODO.objects.filter(user=request.user).order_by('-date')
-        return redirect('/todo', {'res' : res })
-    res = models.TODO.objects.filter(user=request.user).order_by('-date')
-    return render(request, 'todo.html', {'res':res})
+        TODO.objects.create(title=title, user=request.user)
+        return redirect('/todo')
 
-def edit_todo(request):
-    pass
+    res = TODO.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'todo.html', {'res': res})
